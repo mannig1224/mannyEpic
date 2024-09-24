d41f72b4412fa9f8ad1fb63a60f48ebcf2e21b61
Emmanuel Gatica <Emmanuel.Gatica@audioenhancement.com>
2024-09-24 17:40:15 -0600

    changed image

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import styles from './Logo.module.css'; // Use 'styles' for CSS Modules
 const Logo = () => {
   return (
     <div className={styles.logoContainer}>
-      <img src="/images/logo.png" alt="Logo" className={styles.logo} />
+      <img src="/images/generic-logo.png" alt="Logo" className={styles.logo} />
     </div>
   );
 };
