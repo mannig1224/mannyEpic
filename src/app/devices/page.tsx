7dc115a638ac091bee964a4a2577dbdeccc6e814
Emmanuel Gatica <Emmanuel.Gatica@audioenhancement.com>
2024-12-13 12:22:13 -0700

    setup device page

--- src/app/devices/page.tsx
+++ src/app/devices/page.tsx
@@ -1,21 +1,59 @@
 import React from 'react';
 
-import styles from '../../styles/Devices.module.css';
+import styles from './devices.module.css';
+import DevicesTable from '../../components/devicesPageComponents/DevicesTable/DevicesTable';
 
 const Devices: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div>Nav Section</div>.
-      <main className={styles.main}>
-        <h1 className={styles.title}>Devices</h1>
-        <p className={styles.description}>
-          This is the devices page. Here, you can manage your connected devices.
-        </p>
-        <ul className={styles.deviceList}>
-          <li className={styles.device}>Device 1</li>
-          <li className={styles.device}>Device 2</li>
-          <li className={styles.device}>Device 3</li>
+    <div className={styles.mainContent}>
+
+
+      <div className={styles.navSection}>
+      <ul className={styles.optionsList}>
+
+
+          <li className={styles.option}>
+            <div className={`${styles.icon} ${styles.iconDiscover}`}>D</div>
+            <div className={styles.textContent}>
+              <h2 className={styles.title}>Devices</h2>
+              <p className={styles.subtitle}>Devices added to Epic </p>
+            </div>
+          </li>
+          <li className={styles.option}>
+            <div className={`${styles.icon} ${styles.iconAdd}`}>D</div>
+            <div className={styles.textContent}>
+              <h2 className={styles.title}>Discover</h2>
+              <p className={styles.subtitle}>Discover Devices</p>
+            </div>
+          </li>
+          <li className={styles.option}>
+            <div className={`${styles.icon} ${styles.iconConfigure}`}>D</div>
+            <div className={styles.textContent}>
+              <h2 className={styles.title}>Configure</h2>
+              <p className={styles.subtitle}>
+                Configure Devices In Bulk
+              </p>
+            </div>
+          </li>
+          <li className={styles.option}>
+            <div className={`${styles.icon} ${styles.iconReboot}`}>D</div>
+            <div className={styles.textContent}>
+              <h2 className={styles.title}>Reboot</h2>
+              <p className={styles.subtitle}>Reboot Devices</p>
+            </div>
+          </li>
+          <li className={styles.option}>
+            <div className={`${styles.icon} ${styles.iconUpdate}`}>D</div>
+            <div className={styles.textContent}>
+              <h2 className={styles.title}>Update</h2>
+              <p className={styles.subtitle}>Update Devices</p>
+            </div>
+          </li>
         </ul>
+      </div>
+      <main className={styles.main}>
+        
+        <DevicesTable/>
       </main>
     </div>
   );
